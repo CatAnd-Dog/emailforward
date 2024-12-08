aee2c8dbceb0fde09899f61b9b66d72168fa9639
yijia <13262866275@163.com>
2024-12-08 15:36:04 +0800

    main

--- main.py
+++ main.py
@@ -926,6 +926,6 @@ async def receive_mailgun_forward(
     }
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="info")
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="info")
